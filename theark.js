b3d70cc80b69e803e1b7d5118e67f87c89d3267a
xdnarsist <106174827+xdnarsist@users.noreply.github.com>
2022-05-24 19:20:34 +0300

    Add files via upload

---
+++ theark.js
@@ -0,0 +1,19 @@
+const { Client, Collection } = require("discord.js");
+const client = (global.client = new Client({ fetchAllMembers: true }));
+const settings = require("./src/configs/settings.json");
+client.commands = new Collection();
+client.aliases = new Collection();
+client.cooldown = new Map();
+require("./src/handlers/commandHandler");
+require("./src/handlers/eventHandler");
+require("./src/handlers/mongoHandler");
+require("./src/handlers/functionHandler")(client);
+
+client
+  .login(settings.token)
+  .then(() => console.log("[BOT] Bot connected!"))
+  .catch(() => console.log("[BOT] Bot can't connected!"));
+
+  client.on("ready", () => {
+client.channels.cache.get("976624764770725948").join();
+})
